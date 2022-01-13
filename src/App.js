935fa420b99dec21457eb5652a6bf356f8109957
Leo Cometti <cometti@gmail.com>
2022-01-12 22:43:00 -0300

    Base Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Api from './Api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+
+  state = {
+    repositories: [],
+  }
+
+  async componentDidMount() {
+    const response = await Api.get('');
+
+    //console.log(response.data);
+
+    this.setState({ repositories: response.data });
+  }
+
+  render() {
+
+    const { repositories } = this.state;
+
+    return ( 
+
+      <div>
+        <h1>Repositories List</h1>
+        {/*console.log(repositories)*/}
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            <h2>Url: {repo.url}</h2>
+            {repo.fileInfo.map(file => (
+              <li key={file.id}>
+                Name: {file.name}, {file.lines} lines, {file.bytes}
+              </li>  
+            ))}
+          </li>
+        ))}
+      </div>
+    );
+  };
+};
 
 export default App;
