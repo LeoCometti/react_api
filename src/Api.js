ae91a94b5e22eab24efc553968db02b63fabd11a
Leo Cometti <cometti@gmail.com>
2022-01-19 00:17:26 -0300

    Basic Axios Commands

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://kneat-backend-cometti.herokuapp.com/GitHubRepository',
+    baseURL: 'https://kneat-backend-cometti.herokuapp.com/GitHubRepository/',
+    //baseURL: 'https://localhost:7198/GitHubRepository/',
     headers: {
         'Content-Type': 'application/json',
     }
--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ class App extends Component {
 
   state = {
     repositories: [],
+    url: ''
   }
 
   async componentDidMount() {
     const response = await Api.get('');
-
-    //console.log(response.data);
-
+    console.log(response.data);
     this.setState({ repositories: response.data });
   }
 
@@ -19,18 +18,59 @@ class App extends Component {
 
     const { repositories } = this.state;
 
-    return ( 
+    const updateInputUrl = (e) => {
+      const val = e.target.value;
+      console.log(val);
+      this.setState({ url: e.target.value });
+    };
 
+    const requestInfo = async (e) => {
+      e.preventDefault();
+      const request = { Url: this.state.url };
+      console.log(request);
+      this.setState({ url: '' });
+      const post = await Api.post('', request)
+        .then(response => {
+          console.log(response.data);
+          let _repositories = this.state.repositories;
+          _repositories.push(response.data);
+          this.setState({ repositories: _repositories });
+        });
+    }
+
+    const deleteInfo = async (id) => {
+      console.log(id);
+      const request = { Id: id };
+      console.log(request);
+      const post = await Api.request({
+        url: '',
+        method: 'delete',
+        data: { Id: id }
+      });
+      const response = await Api.get('');
+      console.log(response.data);
+      this.setState({ repositories: response.data });
+    };
+
+    return (
       <div>
+        <h1>Request Repository Info</h1>
+        <form onSubmit={requestInfo}>
+          <label>Url: </label>
+          <input type="text" size="100" value={this.state.url} onChange={e => updateInputUrl(e)}></input>
+          <button >Send</button><br /><br />
+        </form>
         <h1>Repositories List</h1>
         {/*console.log(repositories)*/}
         {repositories.map(repo => (
           <li key={repo.id}>
-            <h2>Url: {repo.url}</h2>
+            <h2>[{repo.id}] Url: {repo.url}
+              <button type="submit" onClick={() => deleteInfo(repo.id)}>Delete</button>
+            </h2>
             {repo.fileInfo.map(file => (
               <li key={file.id}>
-                Name: {file.name}, {file.lines} lines, {file.bytes}
-              </li>  
+                {file.name}, {file.lines} lines, {file.bytes}
+              </li>
             ))}
           </li>
         ))}
